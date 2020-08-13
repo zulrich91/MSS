f0efd676fb2b5689e25d924c9287e7dc8d103155
Ulrich Tchuenkam <ulrichmobi2@gmail.com>
2020-08-13 19:53:50 +0200

    File: Twitter developer and app credentials
    In this file we provide the API and TOKEN keys.

---
+++ credentials.py
@@ -0,0 +1,9 @@
+# Go to http://apps.twitter.com and create an app.
+# The consumer key and secret will be generated for you
+API_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+API_SECRET_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+# After the step above, you will be redirected to your app's page.
+# Create an access token under the the "Your access token" section
+ACCESS_TOEKN = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+ACCESS_TOKEN_SECRET = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
