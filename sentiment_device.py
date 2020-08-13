e8ce6f06300cb70400be8dcadce0dbca84fcc4ba
Ulrich Tchuenkam <ulrichmobi2@gmail.com>
2020-08-13 23:49:32 +0200

    Add: Save polarity and subjectivity score.
    Add support to store the score and labels of the polarity and subjectivity of a tweet.

--- sentiment_device.py
+++ sentiment_device.py
@@ -3,7 +3,7 @@ import pyspark.sql.functions as psf
 from pyspark.sql import SparkSession
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StringType
+from pyspark.sql.types import StringType, FloatType
 from textblob import TextBlob
 
 output_path = 'agg/'
@@ -13,8 +13,14 @@ spark = SparkSession \
     .appName("Spark Tweet") \
     .getOrCreate()
 
+@psf.udf(FloatType())
+def udf_get_polarity(tweet):
+    tweet_blob = TextBlob(str(tweet))
+    sentiment = tweet_blob.sentiment
+    return sentiment.polarity
+    
 @psf.udf(StringType())
-def udf_get_sentiment(tweet):
+def udf_polarity_label(tweet):
     tweet_blob = TextBlob(str(tweet))
     sentiment = tweet_blob.sentiment
     if sentiment.polarity > 0: 
@@ -23,14 +29,44 @@ def udf_get_sentiment(tweet):
         return 'neutral'
     else: 
         return 'negative'
+    
+@psf.udf(FloatType())
+def udf_get_subj(tweet):
+    tweet_blob = TextBlob(str(tweet))
+    sentiment = tweet_blob.sentiment
+    return sentiment.subjectivity
+    
+@psf.udf(StringType())
+def udf_subj_label(tweet):
+    tweet_blob = TextBlob(str(tweet))
+    sentiment = tweet_blob.sentiment
+    if sentiment.polarity > 0.5: 
+        return 'subjective'
+    elif sentiment.polarity == 0.5: 
+        return 'neutral'
+    else: 
+        return 'objective'
 
 # The input csv file is one with cleaned tweets. 
 # Saves aggregated data on sentiments
 # Saves aggregated data on device types
+
 def source_sentiment(csv_file):
     clean_df = spark.read.csv(csv_file, header=True, sep=',')
-    clean_df2 = clean_df.withColumn("Sentiment", udf_get_sentiment(clean_df.Tweet))
-    sentiment = clean_df2.groupBy('Sentiment').count()
-    sentiment.toPandas().to_csv(output_path+'sentiment_spark.csv', index=False)
-    source = clean_df2.groupBy('Source').count()
+    
+    polarity_df = clean_df.withColumn("polarity", udf_get_polarity(clean_df.Tweet))
+    polarity_df.toPandas().to_csv(output_path+'polarity_spark.csv', index=False)
+
+    source = clean_df.groupBy('Source').count()
     source.toPandas().to_csv(output_path+'device_spark.csv', index=False)
+
+    pol_df = clean_df.withColumn("polarity_label", udf_polarity_label(clean_df.Tweet))
+    pol_label = pol_df.groupBy('polarity_label').count()
+    pol_label.toPandas().to_csv(output_path+'polarity_label.csv', index=False)
+
+    subj_df = clean_df.withColumn("subj_label", udf_subj_label(clean_df.Tweet))
+    subj_label = subj_df.groupBy('subj_label').count()
+    subj_label.toPandas().to_csv(output_path+'subj_label.csv', index=False)
+
+    subj = clean_df.withColumn("subjectivity", udf_get_subj(clean_df.Tweet))
+    subj.toPandas().to_csv(output_path+'subjectivity.csv', index=False)
