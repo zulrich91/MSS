f6cbb30f9bb4bc7f24e722b5f112a72f29bc0331
Ulrich Tchuenkam <ulrichmobi2@gmail.com>
2020-08-14 02:13:43 +0200

    Update:Categorize sentiment and device in percent.

--- sentiment_device.py
+++ sentiment_device.py
@@ -1,6 +1,7 @@
 import pyspark
 import pyspark.sql.functions as psf
 from pyspark.sql import SparkSession
+from pyspark.sql.window import Window
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StringType, FloatType
@@ -58,14 +59,17 @@ def source_sentiment(csv_file):
     polarity_df.toPandas().to_csv(output_path+'polarity_spark.csv', index=False)
 
     source = clean_df.groupBy('Source').count()
+    source = source.withColumn('percent', psf.col('count')*100/psf.sum('count').over(Window.partitionBy()))
     source.toPandas().to_csv(output_path+'device_spark.csv', index=False)
 
     pol_df = clean_df.withColumn("polarity_label", udf_polarity_label(clean_df.Tweet))
     pol_label = pol_df.groupBy('polarity_label').count()
+    pol_label = pol_label.withColumn('percent', psf.col('count')*100/psf.sum('count').over(Window.partitionBy()))
     pol_label.toPandas().to_csv(output_path+'polarity_label.csv', index=False)
 
     subj_df = clean_df.withColumn("subj_label", udf_subj_label(clean_df.Tweet))
     subj_label = subj_df.groupBy('subj_label').count()
+    subj_label = subj_label.withColumn('percent', psf.col('count')*100/psf.sum('count').over(Window.partitionBy()))
     subj_label.toPandas().to_csv(output_path+'subj_label.csv', index=False)
 
     subj = clean_df.withColumn("subjectivity", udf_get_subj(clean_df.Tweet))
